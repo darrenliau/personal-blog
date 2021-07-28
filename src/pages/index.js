664f5255c255a0480705df29f76607f3a3b16faf
Ming Sheng Liau <70752982+darrenliau@users.noreply.github.com>
2021-07-29 11:43:58 +1200

    updates Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ const IndexPage= () => {
       <Layout>
       <h1>Hello!</h1>
       <h2>I'm Darren, a soon to be graduate with a Computer Science major in Auckland.</h2>
-      <p>Need a developer? <Link to='/mycontacts'>Contact me</Link></p>
       <p>Want to know more about me? <Link to='/aboutme'>Click Me!</Link></p>
       </Layout>
     </div>
