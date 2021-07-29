1a0098992026b0f5b7f883832f2ffb1412698e73
Ming Sheng Liau <70752982+darrenliau@users.noreply.github.com>
2021-07-29 13:02:47 +1200

    added link to mail to email

--- src/pages/mycontacts.js
+++ src/pages/mycontacts.js
@@ -9,7 +9,7 @@ const contactPage = () =>{
             <Layout>
             <h1>Contact Details</h1>
             <p>Phone Number: +64220288943</p>
-            <p>Email: darren6250@icloud.com</p>
+            <p>Email: <a href={'mailto:' + 'darren6250@icloud.com'}>darren6250@icloud.com</a></p>
             <p>GitHub Link: <a href="https://github.com/darrenliau">https://github.com/darrenliau</a></p>
             <p>LinkedIn: <a href="https://www.linkedin.com/in/ming-sheng-liau-2b80401b4/">https://www.linkedin.com/in/ming-sheng-liau-2b80401b4/</a></p>
             <p>Want to know more about me? <Link to='/aboutme'>Click Me!</Link></p>
